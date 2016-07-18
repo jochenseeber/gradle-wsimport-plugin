b1f4edf3608757b77fb2ba793a0d20ed5bd35397
wooshie <wooshie@gmail.com>
2016-07-18 15:23:39 +0300

    Fix for invalid packageName generation on Windows
    Replaced hardcoded  separator "/" with platform dependant File.separator.

--- src/main/groovy/me/seeber/gradle/wsimport/WsimportTask.groovy
+++ src/main/groovy/me/seeber/gradle/wsimport/WsimportTask.groovy
@@ -64,7 +64,7 @@ public class WsimportTask extends DefaultTask {
                 JavaExecAction action = getActionFactory().newJavaExecAction()
 
                 File relativeSourceFile = new File(inputDir.toURI().relativize(change.file.toURI()).toString())
-                String packageName = relativeSourceFile.parent.replace("/", ".")
+                String packageName = relativeSourceFile.parent.replace(File.separator, ".")
 
                 Map<String, Object> options = (Map<String, Object>)[
                     "quiet": true,
